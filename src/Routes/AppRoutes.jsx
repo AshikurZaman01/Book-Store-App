55bef42448a73c25afd88cee7ae4f65219aefe4f
AshikurZaman01 <ashikurzaman774@gmail.com>
2025-01-01 15:51:59 +0600

    add Banner

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,5 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Components/Pages/Home/Home'
+import Orders from '../Components/Pages/Orders/Orders'
+import Cart from '../Components/Pages/Cart/Cart'
+import Checkout from '../Components/Pages/Checkout/Checkout'
+import Dashboard from '../Components/Pages/Dashboard/Dashboard'
 
 
 const AppRoutes = () => {
@@ -7,6 +11,10 @@ const AppRoutes = () => {
 
         <Routes>
             <Route path='/' element={<Home></Home>} />
+            <Route path='/orders' element={<Orders></Orders>} />
+            <Route path='/cart' element={<Cart></Cart>} />
+            <Route path='/checkout' element={<Checkout></Checkout>} />
+            <Route path='/dashboard' element={<Dashboard></Dashboard>} />
 
         </Routes>
 
